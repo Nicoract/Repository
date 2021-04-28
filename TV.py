33ad4f1f9ea54ba8cf997359aea30c6f5560f626
Nicoract <45312145+Nicoract@users.noreply.github.com>
2021-04-28 15:28:55 +0300

    Add files via upload

---
+++ TV.py
@@ -0,0 +1,53 @@
+class TV
+    model = ""
+    price = 100
+    efficiency = True
+    turned = False
+
+    def start(self):
+        if (self.turned)
+            print("Телевизор уже включён")
+        else
+            print("Телевизор включён")
+            self.turned=True
+
+    def get_model(selfself):
+        print(self.model)
+
+    def get_price(self):
+        print(self.price)
+
+    def get_efficiency(self):
+        print(self.efficiency)
+
+class Customer
+
+    def get_name(self):
+        print(self.full_name)
+
+    def customer_enter(self, full_name):
+        self.full_name = full_name
+
+class Manager
+
+    def manager_enter(self, full_name, position):
+        self.position = position
+        self.full_name = full_name
+
+    def get_name(self):
+        print(self.full_name)
+
+    def get_position(self):
+        print (self.position)
+
+    def choose_tv(self, chosen_tv):
+        self.chosen_tv = chosen_tv
+
+    def start_tv(self):
+        self.chosen_tv.start()
+
+    def take_tv(self):
+        print(self.chosen_tv)
+
+
+
